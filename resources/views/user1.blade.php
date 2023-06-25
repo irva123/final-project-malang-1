f10c82c850a474019a87188f79f1209de53de66e
nabillah5 <90024977+nabillah5@users.noreply.github.com>
2023-06-26 00:09:04 +0700

    user1dan2

---
+++ resources/views/user1.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.main')
+
+@section('navbar')
+<!-- Section0 Area Start -->
+
+	<div class="latest-news pt-50 pb-150">
+		<div class="container">
+			<div class="row">
+      <div class="col-xs-12">
+            <div class="section-title text-center">
+						<h3><span class="black-text">EVENT</span></h3>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-4 col-md-6">
+					<div class="single-latest-news">
+						<a href="single-news.html"><div class="latest-news-bg news-bg-1"></div></a>
+						<div class="news-text-box">
+							<h3><a href="single-news.html">Rapat Bandung</a></h3>
+							<p class="blog-meta">
+								<span class="ti-calendar"><i class="fas fa-calendar"></i> 27 December, 2019</span>
+							</p>
+							<p class="excerpt">Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.</p>
+							<div class="text-center">
+                            <a href="single-news.html" class="boxed-btn2">read more </i></a>
+						</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="single-latest-news">
+						<a href="single-news.html"><div class="latest-news-bg news-bg-2"></div></a>
+						<div class="news-text-box">
+							<h3><a href="single-news.html">Festival Musik</a></h3>
+							<p class="blog-meta">
+								<span class="ti-calendar"><i class="fas fa-calendar"></i> 27 December, 2019</span>
+							</p>
+							<p class="excerpt">Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.</p>
+							<div class="text-center">
+                            <a href="single-news.html" class="boxed-btn2">read more </i></a>
+						</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0">
+					<div class="single-latest-news">
+						<a href="single-news.html"><div class="latest-news-bg news-bg-3"></div></a>
+						<div class="news-text-box">
+							<h3><a href="single-news.html">Budaya</a></h3>
+							<p class="blog-meta">
+								<span class="ti-calendar"><i class="fas fa-calendar"></i> 27 December, 2019</span>
+							</p>
+							<p class="excerpt">Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.</p>
+							<div class="text-center">
+                            <a href="single-news.html" class="boxed-btn2">read more </i></a>
+						</div>
+                        </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+  @endsection
+  </html>
---
+++ resources/views/user2.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.main')
+
+@section('navbar')
+<!-- Section0 Area Start -->
+
+	<div class="latest-news pt-50 pb-150">
+		<div class="container">
+			<div class="row">
+      <div class="col-xs-12">
+            <div class="section-title text-center">
+						<h3><span class="black-text">Budaya Bantengan</span></h3>
+					</div>
+			</div>
+            <section>
+
+            <div class="row">
+                <div class="col-6">
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text black-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+
+            
+            <div class="container">
+            <div class="col-md-4 .ms-auto">
+                <button class="btn btn-primary" type="button">Button</button>
+                <button class="btn btn-primary" type="button">Button</button>
+            </div>
+            </div>
+            
+                
+				</div>
+            </section>
+				<div class="col-lg-4 col-md-6">
+					<div class="single-latest-news">
+						<a href="single-news.html"><div class="latest-news-bg news-bg-2"></div></a>
+						<div class="news-text-box">
+							<h3><a href="single-news.html">Festival Musik</a></h3>
+							<p class="blog-meta">
+								<span class="ti-calendar"><i class="fas fa-calendar"></i> 27 December, 2019</span>
+							</p>
+							<p class="excerpt">Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.</p>
+							<div class="text-center">
+                            <a href="single-news.html" class="boxed-btn2">read more </i></a>
+						</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0">
+					<div class="single-latest-news">
+						<a href="single-news.html"><div class="latest-news-bg news-bg-3"></div></a>
+						<div class="news-text-box">
+							<h3><a href="single-news.html">Budaya</a></h3>
+							<p class="blog-meta">
+								<span class="ti-calendar"><i class="fas fa-calendar"></i> 27 December, 2019</span>
+							</p>
+							<p class="excerpt">Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.</p>
+							<div class="text-center">
+                            <a href="single-news.html" class="boxed-btn2">read more </i></a>
+						</div>
+                        </div>
+					</div>
+				</div>
+                <div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0">
+					<div class="single-latest-news">
+						<a href="single-news.html"><div class="latest-news-bg news-bg-3"></div></a>
+						<div class="news-text-box">
+							<h3><a href="single-news.html">Budaya</a></h3>
+							<p class="blog-meta">
+								<span class="ti-calendar"><i class="fas fa-calendar"></i> 27 December, 2019</span>
+							</p>
+							<p class="excerpt">Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.</p>
+							<div class="text-center">
+                            <a href="single-news.html" class="boxed-btn2">read more </i></a>
+						</div>
+                        </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+  @endsection
+  </html>
